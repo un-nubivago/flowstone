d753f13d6aefe8157958b1f7e6c4819ce39392aa
Nubìvago <fradige95@gmail.com>
2026-05-09 11:05:28 +0200

    Fix configuration loading logic

--- src/main/java/niv/flowstone/config/Configuration.java
+++ src/main/java/niv/flowstone/config/Configuration.java
@@ -12,9 +12,20 @@ import net.fabricmc.fabric.api.event.EventFactory;
 @NullMarked
 public final class Configuration {
 
-    public static final Event<@NonNull Runnable> LOADED = EventFactory
-            .createArrayBacked(Runnable.class,
-                    runnables -> () -> Stream.of(runnables).forEach(Runnable::run));
+    public static final Event<@NonNull Runnable> LOADED;
+
+    private static final Loader<@NonNull Configuration> LOADER;
+
+    static {
+        LOADED = EventFactory.createArrayBacked(
+                Runnable.class,
+                runnables -> () -> Stream.of(runnables).forEach(Runnable::run));
+
+        LOADER = new Loader<>(
+                Configuration.class,
+                () -> LOADED.invoker().run(),
+                Configuration::new);
+    }
 
     private boolean allowDeepslateGenerators = true;
     private boolean allowWorldlyGenerators = true;
@@ -34,7 +45,7 @@ public final class Configuration {
     }
 
     private static final Configuration getInstance() {
-        return ConfigurationLoader.getConfiguration();
+        return LOADER.getConfiguration();
     }
 
     public static final void init() {
